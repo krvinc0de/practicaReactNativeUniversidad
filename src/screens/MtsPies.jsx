a8f89e784e7af37d592529bf0dc9cd1ca6a55f99
Kevin Medina <79812923+krvinc0de@users.noreply.github.com>
2023-02-14 14:26:52 -0600

    terminado
    funcion terminada

--- src/screens/MtsPies.jsx
+++ src/screens/MtsPies.jsx
@@ -4,9 +4,45 @@ import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
 const MtsPies = () => {
+
+    const [entry, setEntry] = useState();
+    const [final, setFinal] = useState();
+
+    const convert = () => {
+        let res= parseFloat(entry) * 3.281;
+        setFinal(res)
+        console.log(final);
+        return final
+    };
+
     return (
         <View style={styles.container}>
-            <Text>MtsPies</Text>
+            <Text style={styles.titulo}>Metros a pies</Text>
+            <Text style={styles.subtitulo}>Ingrese los metros a convertir</Text>
+            <TextInput
+                keyboardType='numeric'
+                inputMode='decimal'
+                style={styles.inputEntry}
+                placeholder='ingrese a metros'
+                onChangeText={(value)=>{
+                    setEntry(value)
+                }}  
+            />
+            <TouchableOpacity 
+            onPress={convert}
+            style={styles.boton}>
+                <Text>Convertir</Text>
+            </TouchableOpacity>
+            <Text style={styles.subtitulo}>Resultado de metros a pies</Text>   
+            {
+                final === undefined || isNaN(final) ? <View/> : (
+                    <TextInput 
+                    style={styles.inputEntry}
+                    value={final.toString()}
+                />
+                )
+            }
+            
         </View>
     );
 };
@@ -15,10 +51,25 @@ const MtsPies = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        backgroundColor: '#fff',
     },
+    titulo: {
+        color: "#000",
+        fontSize: 20
+    },
+    subtitulo: {
+        color: "#000",
+        fontSize: 15
+    },
+    inputEntry: {
+        borderColor: '#000',
+        borderWidth: 1
+    },
+    boton: {
+        backgroundColor: 'grey'
+    }
+
 });
 
 //make this component available to the app
